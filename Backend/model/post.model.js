86e75d7b4f9ab9d111d7132c2c388ab239b6f5b1
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2023-05-02 16:50:41 +0530

    -()

--- Backend/model/post.model.js
+++ Backend/model/post.model.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 
 const postSchema = mongoose.Schema({
     title:{type:String,require:true},
+    
 body:{type:String,require:true},
 device:{type:String,enum:["PC","TABLET","MOBILE"],require:true},
 userId:{type:String,require:true}
